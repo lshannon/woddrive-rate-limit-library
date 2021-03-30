c66c2b1c65d534fd64fba30bf792d3377f63db38
lshannon <luke.shannon@gmail.com>
2021-03-30 13:10:15 -0400

    First check in

---
+++ src/main/java/com/woddrive/activity/RateLimitingAspect/RateLimitingAspectApplication.java
@@ -0,0 +1,13 @@
+package com.woddrive.activity.RateLimitingAspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitingAspectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimitingAspectApplication.class, args);
+	}
+
+}
